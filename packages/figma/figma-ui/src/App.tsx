d2d1c9f9b9af78056f51878943e263e502db2832
shuhao02 <shuhao02@baidu.com>
2024-01-15 20:02:44 +0800

    netdisk-infrastructure-1471
    Change-Id: I425034561ad3ad81a2763bf2cb309d16d93588cb

--- packages/figma/figma-ui/src/App.tsx
+++ packages/figma/figma-ui/src/App.tsx
@@ -1,26 +1,49 @@
-import { Typography } from '@mui/material';
-import { FC, useEffect, useState } from 'react';
-import VerticalStepper from './components/VerticalStepper';
+import { FC } from 'react';
+import { css } from '@emotion/react';
+import {
+  Avatar,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  ListSubheader,
+  MenuItem,
+  Select,
+  Stack,
+} from '@mui/material';
+import { AddCircleOutline } from '@mui/icons-material';
+const containerStyle = css({
+  margin: '10px auto',
+  paddingTop: 30,
+  maxWidth: 370,
+  height: '100vh',
+  border: '1px solid #cccccc',
+  display: 'flex',
+  justifyContent: 'center',
+});
 
 const App: FC = () => {
-  const [data, setData] = useState<any>(null);
-
-  useEffect(() => {
-    window.addEventListener('message', (event) => {
-      console.log('listen to event data', event.data);
-      setData(event.data.pluginMessage);
-    });
-  });
   return (
-    <>
-      <Typography
-        variant="h5"
-        style={{ textAlign: 'center', marginBottom: 20 }}
-      >
-        Figma Netdisk D2C
-      </Typography>
-      <VerticalStepper data={data} />
-    </>
+    <div css={containerStyle}>
+      <Stack spacing={2}>
+        <FormControl size="small">
+          <AddCircleOutline
+            sx={{
+              width: 36,
+              height: 36,
+            }}
+          />
+          <InputLabel id="project-input-label">project</InputLabel>
+          <Select labelId="project-input-label" label="project">
+            <ListSubheader>Vue</ListSubheader>
+            <MenuItem>Ten</MenuItem>
+            <MenuItem>Ten</MenuItem>
+            <ListSubheader>React</ListSubheader>
+            <MenuItem>sdfds</MenuItem>
+          </Select>
+          <FormHelperText>Select a project</FormHelperText>
+        </FormControl>
+      </Stack>
+    </div>
   );
 };
 
--- packages/figma/figma-ui/vite.config.ts
+++ packages/figma/figma-ui/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    viteSingleFile(),
+  ],
   build: {
     outDir: '../figma-main/dist',
   },
